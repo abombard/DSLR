71530140d483075b0a32d00e5d1e098c6d3d93b6
Arthur Bombarde <arthbab@gmail.com>
2019-05-05 15:21:02 +0200

    WIP: graphic interface

--- histogram.py
+++ histogram.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.7
 
 import sys
+import matplotlib
 import matplotlib.pyplot as plt
 
 from utils import error
@@ -15,6 +16,15 @@ if __name__ == '__main__':
 
     stats = describe.stats(sys.argv[1])
     cvs = { name : stat["Std"] / stat["Mean"] for name, stat in stats.items() }
-    print(cvs)
 
-    
+    # Generate a normal distribution, center at x=0m y=5
+    fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+    x = [s for n, s in cvs.items()]
+    y = [n for n, s in cvs.items()]
+    n_bins = len(x)
+
+    axs[0].hist(x, bins=n_bins)
+    axs[1].hist(y, bins=n_bins)
+
+    plt.show()
