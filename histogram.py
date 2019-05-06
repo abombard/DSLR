a2cb415658fcd11c33339a1e4dbdbf26ec1edbd1
Arthur Bombarde <arthbab@gmail.com>
2019-05-06 20:13:06 +0200

    histogram: (wip) stacked bar

--- histogram.py
+++ histogram.py
@@ -1,30 +1,89 @@
 #!/usr/bin/python3.7
 
 import sys
+import os
+import csv
 import matplotlib
 import matplotlib.pyplot as plt
 
+import utils
 from utils import error
 import describe
 
 def usage():
     error('%s [dataset]' % sys.argv[0])
 
+def count_elem_in(List, Min, Max):
+    count = 0
+    for elem in List:
+        if Min <= elem and elem <= Max:
+            count += 1
+    print("COUCOU: %s, %s" % (Min, Max))
+    return count
+
+def histogram(course, houses):
+    stats = describe.stats(sys.argv[1])
+    total = stats[course]["Count"]
+    lims = [
+       stats[course]["min"],
+       stats[course]["25%"],
+       stats[course]["50%"],
+       stats[course]["75%"],
+       stats[course]["max"],
+    ]
+
+    legend = [[], []]
+    ind = [x for x in range(4)]
+    bottom = [0 for x in range(4)]
+    for house, notes in houses.items():
+        counts = [count_elem_in(notes, lims[i], lims[i+1]) / total * 100 for i in range(4)]
+        p = plt.bar(ind, counts, bottom=bottom, align='edge')
+        bottom = [bottom[i] + counts[i] for i in range(4)]
+        legend[0] += [p[0]]
+        legend[1] += [house]
+
+    plt.title(course)
+    plt.xticks([x for x in range(5)], (
+        "%.3f" % stats[course]["min"],
+        "%.3f" % stats[course]["25%"],
+        "%.3f" % stats[course]["50%"],
+        "%.3f" % stats[course]["75%"],
+        "%.3f" % stats[course]["max"])
+    )
+    plt.legend(tuple(legend[0]), tuple(legend[1]))
+    plt.show()
+
+def main_pichu(filename):
+    # checks
+    if not os.path.isfile(filename):
+        error('no such file: %s' % filename)
+
+    header = []
+    features = []
+
+    # parser: csv to feature lists
+    with open(filename, 'r') as fs:
+        reader = csv.reader(fs)
+        header = reader.__next__()
+        header = header[6:]
+        features = [ {"Ravenclaw": [], "Slytherin": [], "Gryffindor": [], "Hufflepuff":[] } for i in range(len(header)) ]
+        is_numeric = [ False for i in range(len(header)) ]
+        for line in reader:
+            for i, field in enumerate(line):
+                if i == 1:
+                    house = field
+                elif i >= 6 and field != "":
+                    features[i - 6][house] += [float(field)]
+
+    return (header, features)
+
 if __name__ == '__main__':
+    # checks
     if len(sys.argv) != 2:
         usage()
+    if not os.path.isfile(sys.argv[1]):
+        error('no such file: %s' % sys.argv[1])
 
-    stats = describe.stats(sys.argv[1])
-    cvs = { name : stat["Std"] / stat["Mean"] for name, stat in stats.items() }
-
-    # Generate a normal distribution, center at x=0m y=5
-    fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
-
-    x = [s for n, s in cvs.items()]
-    y = [n for n, s in cvs.items()]
-    n_bins = len(x)
-
-    axs[0].hist(x, bins=n_bins)
-    axs[1].hist(y, bins=n_bins)
-
-    plt.show()
+    header, features = main_pichu(sys.argv[1])
+    index = 11
+    histogram(header[index], features[index])
--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3.7
 
 import sys
+import re
 
 def error(*args):
     print('error: %s' % args, file=sys.stderr)
     sys.exit(1)
+
+re_dig = re.compile(r"[-]?\d+\.\d*")
+def is_digit(s):
+    return re_dig.match(s)
 
