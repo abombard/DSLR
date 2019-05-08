b68c47dafeb6eab165ccbd3d8d41810642d6a789
Arthur Bombarde <arthbab@gmail.com>
2019-05-08 16:58:43 +0200

    scatter_plot: 0 values are ignored in f1 and f2

--- scatter_plot.py
+++ scatter_plot.py
@@ -12,6 +12,20 @@ import describe
 def usage():
     error('%s [dataset]' % sys.argv[0])
 
+def scatter(h1, f1, h2, f2):
+    f1 = copy.copy(f1)
+    f2 = copy.copy(f2)
+
+    to_pop = [i for i in range(len(f1)) if not f1[i] or not f2[i]]
+    f1 = [x for i, x in enumerate(f1) if i not in to_pop]
+    f2 = [x for i, x in enumerate(f2) if i not in to_pop]
+
+    plt.scatter(f1, f2, alpha=0.8, c="green", edgecolor='none', s=30)
+    plt.xlabel(h1)
+    plt.ylabel(h2)
+    plt.show()
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         usage()
@@ -23,13 +37,7 @@ if __name__ == "__main__":
     features = features[6:]
 
     i1, i2 = 1, 4
-    f1 = describe.clean_feature(features[i1], empty_is_valid=True)
-    f2 = describe.clean_feature(features[i2], empty_is_valid=True)
-
-    plt.scatter(f1, f2, alpha=0.8, c="green", edgecolor='none', s=30)
-    plt.xlabel(headers[i1])
-    plt.ylabel(headers[i2])
-    plt.show()
+    scatter(headers[i1], features[i1], headers[i2], features[i2])
     sys.exit(0)
 
     # test all features
@@ -37,17 +45,8 @@ if __name__ == "__main__":
     for i1 in range(imax-1):
         h1 = headers[i1]
         for i2 in range(i1+1, imax):
-            f1 = copy.copy(features[i1])
+            f1 = features[i1]
             h2 = headers[i1 + i2]
-            f2 = copy.copy(features[i1 + i2])
-
-            to_pop = [i for i in range(len(f1)) if not f1[i] or not f2[i]]
-            f1 = [x for i, x in enumerate(f1) if i not in to_pop]
-            f2 = [x for i, x in enumerate(f2) if i not in to_pop]
-
-            plt.scatter(f1, f2, alpha=0.8, c="green", edgecolor='none', s=30)
-            plt.xlabel(h1)
-            plt.ylabel(h2)
-            plt.show()
-
+            f2 = features[i1 + i2]
+            scatter(h1, f1, h2, f2)
     sys.exit(0)
