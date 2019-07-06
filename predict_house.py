bcb7e78817d984c8e2bed534ec832389e9f32b66
Git-Math <matoo3@hotmail.fr>
2019-07-06 15:42:29 +0200

    add predict_house.py

---
+++ predict_house.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import numpy as np
+
+import utils
+from utils import error
+import file
+import logreg_train
+import logreg_predict
+
+feature_number = 13
+
+def usage():
+    error('%s [theta dataset]' % sys.argv[0])
+
+def ask_marks():
+    mark_matrix = np.empty([feature_number + 1, 1])
+    mark_matrix[0][0] = 1.0
+    subject_list = ["Arithmancy", "Astronomy", "Herbology",             \
+    "Defense Against the Dark Arts", "Divination", "Muggle Studies",    \
+    "Ancient Runes", "History of Magic", "Transfiguration", "Potions",  \
+    "Care of Magical Creatures", "Charms", "Flying"]
+    i = 1
+    print("Fill your marks")
+    for subject in subject_list:
+        loop = True
+        while loop:
+            try:
+                mark = float(input(subject + ": "))
+                if mark < 0.0 or mark > 20.0:
+                    raise Exception()
+                mark_matrix[i][0] = mark / 20.0
+                i += 1
+                loop = False
+            except:
+                print("mark must be a number between 0 and 20")
+    return mark_matrix
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+    if not os.path.isfile(sys.argv[1]):
+        error('no such file: %s' % sys.argv[1])
+    mark_matrix = ask_marks()
+    theta_data = file.read_theta(sys.argv[1], feature_number + 1)
+    houses = logreg_predict.logreg_predict(mark_matrix, theta_data)
+    print("Sorting Hat: Hum, you will go to... " + houses[0] + "!")
+    
